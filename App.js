0ee6ba6eb2ecd2cfe212eca094f631ece1eb2641
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-16 19:25:00 +0100

    Adding physics

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from './Constants';
+import { GameEngine } from 'react-native-game-engine';
+import Matter from 'matter-js';
+import Bird from './Bird';
+import Physics from './Physics';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.GameEngine = null;
+    this.entities = this.setupWorld();
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false });
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50)
+    Matter.World.add(world, [bird])
+
+    return {
+      physics: { engine: engine, world: world },
+      bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird }
+    }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <GameEngine ref={(ref) => { this.GameEngine = ref; }} style={styles.gameContainer} systems={[Physics]} entities={this.entities} />
       </View>
     );
   }
@@ -18,8 +38,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });

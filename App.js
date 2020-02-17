000901772738a804a76bffa57a61b3aa51db3760
hanna_nyman <hannanyman.sk@gmail.com>
2020-02-17 09:42:02 +0100

    Clicking makes the bird jump

--- App.js
+++ App.js
@@ -4,11 +4,13 @@ import Constants from './Constants';
 import { GameEngine } from 'react-native-game-engine';
 import Matter from 'matter-js';
 import Bird from './Bird';
+import Wall from './Wall';
 import Physics from './Physics';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.GameEngine = null;
     this.entities = this.setupWorld();
   }
@@ -17,12 +19,17 @@ export default class App extends React.Component {
     let engine = Matter.Engine.create({ enableSleeping: false });
     let world = engine.world;
 
-    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50)
-    Matter.World.add(world, [bird])
+    let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50 );
+    let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT - 25, Constants.MAX_WIDTH, 50, { isStatic: true } );
+    let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true } );
+
+    Matter.World.add(world, [bird, floor]);
 
     return {
       physics: { engine: engine, world: world },
-      bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird }
+      bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird },
+      floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
+      ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall }
     }
   }
 
